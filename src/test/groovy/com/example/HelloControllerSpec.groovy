d84f15c702b91cfe3221a99c903dd8b6b018abad
ls001 <ls001@ls001ui-MacBook-Pro.local>
2025-05-11 12:23:44 +0900

    add async test

--- src/test/groovy/com/example/HelloControllerSpec.groovy
+++ src/test/groovy/com/example/HelloControllerSpec.groovy
@@ -7,6 +7,7 @@ import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
 import spock.lang.Specification
+import spock.util.concurrent.PollingConditions
 
 @MicronautTest
 class HelloControllerSpec extends Specification {
@@ -25,5 +26,27 @@ class HelloControllerSpec extends Specification {
         then: "the response is not null and contains the expected message"
         response != null
         response == "Hello World"
+    }
+
+    def "test hello endpoint returns correct response asynchronously"() {
+        given: "a request to the hello endpoint and polling conditions"
+        def request = HttpRequest.GET("/hello").accept(MediaType.APPLICATION_JSON)
+        def conditions = new PollingConditions(timeout: 5, delay: 0.1)
+        def response = null
+        def error = null
+
+        when: "the request is executed asynchronously"
+        client.retrieve(request)
+            .subscribe(
+                { result -> response = result },
+                { e -> error = e }
+            )
+
+        then: "the response is not null and contains the expected message"
+        conditions.eventually {
+            assert error == null
+            assert response != null
+            assert response == "Hello World"
+        }
     }
 } 
